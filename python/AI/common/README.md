c060c2948b6c39f658519c183f9e03c3ac329332
ゆま <yumasan.122700@gmail.com>
2025-02-03 13:45:53 +0900

    update

---
+++ python/AI/common/README.md
@@ -0,0 +1,4 @@
+CBOW : continuous bag-of-words → 袋の中にある単語。南湖の並びが無視される
+
+TimeLayers
+time embeddingなどのたtimeレイヤは順伝播時にT個のenbeddingレイヤを用意し、各Embeddingレイヤが書く時刻のデータを処理する
---
+++ python/AI/common/RNN|LSTM/time_layers.py
@@ -0,0 +1,203 @@
+import numpy as np
+from .layers import *
+
+class RNN:
+	"""
+	RNNの1ステップの処理を行う
+	"""
+	def __init__(self, Wx, Wh, b):
+		self.params = [Wx, Wb, b]
+		self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+		self.cache = None
+
+	def forward(self, x, h_prev):
+		Wx, Wh, b = self.params
+		t = np.dot(h_prev, Wh) + np.dot(x, Wx) + b
+		h_next = np.tanh(t)
+
+		self.cache = (x, h_prev, h_next)
+		return h_next
+
+	def backward(self, dh_next):
+		Wx, Wh, b = self.params
+		x, h_prev, h_next = self.cache
+
+		dt = dh_next * (1 - h_next ** 2)
+		db = np.sum(dt, axis=0)
+		dWh = np.dot(h_prev.T, dt)
+		dh_prev = np.dot(dt, Wh.T)
+		dWx = np.dot(x.T, dt)
+		dx = np.dot(dt, Wx.T)
+
+		self.grads[0][...] = dWx
+		self.grads[1][...] = dWh
+		self.grads[2][...] = db
+		return dx, dh_prev
+
+class TimeRNN:
+    """T個のRNNレイヤから構成"""
+    def __init__(self, Wx, Wh, b, stateful=False):
+        self.params = [Wx, Wh, b]
+        self.grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        self.layers = None
+
+        self.h, self.dh = None, None
+        self.stateful = stateful
+
+    def forward(self, xs):
+        """Parameter
+        --------------
+        xs : (N, T, D)  N:バッチサイズ  D:入力ベクトルの次元数 T:時系列データの個数"""
+        Wx, Wh, b = self.params
+        N, T, D = xs.shape
+        D, H = Wx.shape
+
+        self.layers = []
+        hs = np.empty((N, T, H), dtype='f')
+
+        if not self.stateful or self.h is None:
+            self.h = np.zeros((N, H), dtype='f')
+
+        for t in range(T):
+            layer = RNN(*self.params)
+            self.h = layer.forward(xs[:, t, :], self.h)
+            hs[:, t, :] = self.h
+            self.layers.append(layer)
+
+        return hs
+
+    def backward(self, dhs):
+        Wx, Wh, b = self.params
+        N, T, H = dhs.shape
+        D, H = Wx.shape
+
+        dxs = np.empty((N, T, D), dtype='f')
+        dh = np.zeros((N, H), dtype=np.float32)
+        grads = [np.zeros_like(Wx), np.zeros_like(Wh), np.zeros_like(b)]
+        for t in reversed(range(T)):
+            layer = self.layers[t]
+            dx, dh = layer.backward(dhs[:, t, :] + dh)
+            dxs[:, t, :] = dx
+
+            for i, grad in enumerate(layer.grads):
+                grads[i] += grad
+
+        for i, grad in enumerate(grads):
+            self.grads[i][...] = grad
+        self.dh = dh
+
+        return dxs
+
+    def set_state(self, h):
+        self.h = h
+
+    def reset_state(self):
+        self.h = None
+
+
+class TimeEmbedding:
+	def __init__(self, W):
+		self.params = [W]
+		self.grads = [np.zeros_like(W)]
+		self.layers = None
+		self.W = W
+
+	def forward(self, xs):
+		N, T = xs.shape
+		V, D = self.W.shape
+
+		out = np.empty((N, T, D), dtype='f')
+		self.layers = []
+
+		for t in range(T):
+			layer = Embedding(self.W)
+			out[:, t, :]= layer.forward(xs[:, t])
+			self.layers.append(layer)
+		return out
+
+	def backward(self, dout):
+		N, T, D = dout.shape
+
+		grad = 0
+		for t in range(T):
+			layer = self.layers[t]
+			layer.backward(dout[:, t, :])
+			grad += layer.grads[0]
+		self.grads[0][...] = grad
+		return None
+
+
+class TimeAffine:
+	def __init__(self, W, b):
+		self.params = [W, b]
+		self.grads = [np.zeros_like(W), np.zeros_like(b)]
+		self.x = None
+	
+	def forward(self, x):
+		N, T, D = x.shape
+		W, b = self.params
+
+		rx = x.reshape(N*T, -1)
+		out = np.dot(rx, W) + b
+		self.x = x
+		return out.reshape(N, T, -1)
+
+	def backward(self, dout):
+		x = self.x
+		N, T, D = x.shape
+		W, b = self.params
+
+		dout = dout.reshape(N*T, -1)
+		rx = x.reshape(N*T, -1)
+
+		db = np.sum(dout, axis=0)
+		dW = np.dot(rx.T, dout)
+		dx = np.dot(dout, W.T)
+		dx = dx.reshape(*x.shape)
+
+		self.grads[0][...] = dW
+		self.grads[1][...] = db
+
+		return dx
+
+
+class TimeSoftmaxWithLoss:
+    def __init__(self):
+        self.params, self.grads = [], []
+        self.cache = None
+        self.ignore_label = -1
+
+    def forward(self, xs, ts):
+        N, T, V = xs.shape
+
+        if ts.ndim == 3:  # 教師ラベルがone-hotベクトルの場合
+            ts = ts.argmax(axis=2)
+
+        mask = (ts != self.ignore_label)
+
+        # バッチ分と時系列分をまとめる（reshape）
+        xs = xs.reshape(N * T, V)
+        ts = ts.reshape(N * T)
+        mask = mask.reshape(N * T)
+
+        ys = softmax(xs)
+        ls = np.log(ys[np.arange(N * T), ts])
+        ls *= mask  # ignore_labelに該当するデータは損失を0にする
+        loss = -np.sum(ls)
+        loss /= mask.sum()
+
+        self.cache = (ts, ys, mask, (N, T, V))
+        return loss
+
+    def backward(self, dout=1):
+        ts, ys, mask, (N, T, V) = self.cache
+
+        dx = ys
+        dx[np.arange(N * T), ts] -= 1
+        dx *= dout
+        dx /= mask.sum()
+        dx *= mask[:, np.newaxis]  # ignore_labelに該当するデータは勾配を0にする
+
+        dx = dx.reshape((N, T, V))
+
+        return dx
